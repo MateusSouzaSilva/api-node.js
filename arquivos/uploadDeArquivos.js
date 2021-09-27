ddd51e1c436aa8b15cb2b1fd849a3169d1ca7897
Mateus Souza <mateus_souza11@hotmail.com.br>
2021-09-27 17:37:53 -0300

    trocando para o createReadStream no uploadDeArquivos

--- arquivos/uploadDeArquivos.js
+++ arquivos/uploadDeArquivos.js
@@ -1,10 +1,5 @@
 const fs = require('fs');
 
-fs.readFile('./assets/shihtzu.jpeg', (erro, buffer) => {   
-    console.log('A imagem recebeu buffer');
-    console.log(buffer);
-
-    fs.writeFile('./assets/shihtzu2.jpeg', buffer, (erro) => {
-        console.log('imagem foi escrita');
-    })
-});
+fs.createReadStream('./assets/shihtzu.jpeg')
+    .pipe(fs.createWriteStream('./assets/shihtzu-stream.jpeg'))
+    .on('finish', () => console.log('Imagem foi escrita'));
