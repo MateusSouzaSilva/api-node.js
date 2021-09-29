02cd2cce92d5daa756cf769e9752915d6a222a9a
Mateus Souza <mateus_souza11@hotmail.com.br>
2021-09-29 10:53:16 -0300

    adicionando rota e tabela para criar pets

---
+++ models/pets.js
@@ -0,0 +1,20 @@
+const { query } = require('express');
+const res = require('express/lib/response');
+const conexao = require('../infraestrutura/conexao');
+
+class Pet {
+    adiciona(pet, res) {
+        const query = 'INSERT INTO Pets SET ?';
+
+        conexao.query(query, pet, erro => {
+            if(erro) {
+                console.log(erro);
+                res.status(400).json(erro);
+            } else {
+                res.status(200).json(pet);
+            }
+        });
+    }
+}
+
+module.exports = new Pet();
