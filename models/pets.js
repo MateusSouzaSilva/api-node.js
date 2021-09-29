8132cfa539a12bcf28f359193ea9bd726ffc333c
Mateus Souza <mateus_souza11@hotmail.com.br>
2021-09-29 16:56:44 -0300

    criando manipulação de imagem no servidor e aumentando a segurança de entrada de dados

--- models/pets.js
+++ models/pets.js
@@ -1,17 +1,24 @@
-const { query } = require('express');
-const res = require('express/lib/response');
 const conexao = require('../infraestrutura/conexao');
+const uploadDeArquivos = require('../arquivos/uploadDeArquivos');
 
 class Pet {
     adiciona(pet, res) {
         const query = 'INSERT INTO Pets SET ?';
 
-        conexao.query(query, pet, erro => {
+        uploadDeArquivos(pet.imagem, pet.nome, (erro, novoCaminho) => {
+
             if(erro) {
-                console.log(erro);
-                res.status(400).json(erro);
+                res.status(400).json({erro});
             } else {
-                res.status(200).json(pet);
+                const novoPet = {nome: pet.nome, imagem: novoCaminho}
+                conexao.query(query, novoPet, erro => {
+                    if(erro) {
+                        console.log(erro);
+                        res.status(400).json(erro);
+                    } else {
+                        res.status(200).json(novoPet);
+                    }
+                });
             }
         });
     }
