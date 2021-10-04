cfdb5f3b1c9161e8c82d9cc203221ce979e5d78f
Mateus Souza <mateus_souza11@hotmail.com.br>
2021-10-04 20:50:00 -0300

    refatorando o get

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -56,16 +56,8 @@ class Atendimento {
         }
     }
 
-    lista(res) {
-        const sql = 'Select * FROM Atendimentos';
-
-        conexao.query(sql, (erro, resultados) => {
-            if(erro) {
-                res.status(400).json(erro);
-            } else {
-                res.status(200).json(resultados);
-            }
-        });
+    lista() {
+        return repositorio.lista();
     }
 
     buscaPorId(id, res) {
